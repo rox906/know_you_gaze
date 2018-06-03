674d402d219c75863fbe3068b4e378e401ced4aa
rox906 <rox906@sjtu.edu.cn>
2018-06-03 10:17:51 +0800

    finish camera undistort

--- show_feedback/show_feedback.cpp
+++ show_feedback/show_feedback.cpp
@@ -1,34 +1,28 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
-using namespace cv;
-Mat src, src_gray;
-Mat dst, detected_edges;
+#include <sstream>
+
+cv::Mat src, src_gray;
+cv::Mat dst, detected_edges;
 int lowThreshold = 0;
 const int max_lowThreshold = 100;
 const int ratio = 3;
 const int kernel_size = 3;
 const char *window_name = "Edge Map";
-static void CannyThreshold(int, void *)
+
+void detect_and_show(cv::Mat src)
 {
-  blur(src_gray, detected_edges, Size(3, 3));
-  Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
-  dst = Scalar::all(0);
-  src.copyTo(dst, detected_edges);
-  imshow(window_name, dst);
-}
-int main(int argc, char **argv)
-{
-  src = imread("view.jpg"); // Load an image
-  cv::resize(src, src, cv::Size(960, 540));
+  // cv::resize(src, src, cv::Size(960, 540));
   // dst.create(src.size(), src.type());
-  cvtColor(src, src_gray, COLOR_BGR2GRAY);
+  cvtColor(src, src_gray, cv::COLOR_BGR2GRAY);
   // namedWindow( window_name, WINDOW_AUTOSIZE );
   // createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
   // CannyThreshold(0, 0);
   std::vector<std::vector<cv::Point>> my_contours;
   std::vector<cv::Vec4i> hireachy;
-  Canny(src_gray, src_gray, 50, 150);
+  double mythreshold = 100;
+  cv::Canny(src_gray, src_gray, mythreshold, 3 * mythreshold);
   cv::findContours(src_gray, my_contours, hireachy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
   std::vector<std::pair<double, int>> drawid;
   for (unsigned int i = 0; i < my_contours.size(); ++i)
@@ -36,21 +30,56 @@ int main(int argc, char **argv)
     drawid.push_back(std::make_pair(cv::arcLength(my_contours[i], 0), i));
   }
   sort(drawid.begin(), drawid.end(), [](std::pair<double, int> a, std::pair<double, int> b) { return a.first > b.first; });
-  cv::Mat binary = Mat::zeros(src.size(), CV_8UC3);
+  // cv::Mat binary = Mat::zeros(src.size(), CV_8UC3);
   // cv::imshow("raw", binary);
   std::vector<std::pair<double, int>> drawid_area;
   for (unsigned int i = 0; i < 5; ++i)
   {
-    drawid_area.push_back(std::make_pair(cv::contourArea(my_contours[drawid[i].second]),drawid[i].second));
+    drawid_area.push_back(std::make_pair(cv::contourArea(my_contours[drawid[i].second]), drawid[i].second));
   }
   sort(drawid_area.begin(), drawid_area.end(), [](std::pair<double, int> a, std::pair<double, int> b) { return a.first > b.first; });
-  for (unsigned int i = 0; i < drawid_area.size(); ++i)
+  cv::drawContours(src, my_contours, drawid_area[0].second, cv::Scalar::all(255), 1);
+  cv::imshow("drawcontours", src);
+  // for (unsigned int i = 0; i < drawid_area.size(); ++i)
+  // {
+  //   // cv::drawContours(binary, my_contours, drawid[i].second, cv::Scalar::all(255), 1);
+  //   cv::drawContours(src, my_contours, drawid_area[i].second, cv::Scalar::all(255), 1);
+  //   cv::imshow("drawcontours", src);
+  // }
+  // cv::waitKey(0);
+}
+
+int main(int argc, char **argv)
+{
+  cv::Mat frame, camera_matrix, distortion_coefficients, tmp;
+  std::string showfigure = "showfigure";
+  cv::namedWindow(showfigure, CV_WINDOW_NORMAL);
+  cv::setWindowProperty(showfigure, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+  cv::Mat myfigure = cv::Mat::zeros(cv::Size(1920, 1080), CV_8UC3);
+  myfigure = cv::Scalar::all(255);
+  cv::imshow(showfigure, myfigure);
+  cv::VideoCapture capture(0);
+  cv::FileStorage file_storage("out_camera_data.xml", cv::FileStorage::READ);
+  file_storage["camera_matrix"] >> camera_matrix;
+  file_storage["distortion_coefficients"] >> distortion_coefficients;
+  file_storage.release();
+
+  if (capture.isOpened())
   {
-    // cv::drawContours(binary, my_contours, drawid[i].second, cv::Scalar::all(255), 1);
-    cv::drawContours(src, my_contours, drawid_area[i].second, cv::Scalar::all(255), 1);
-    cv::imshow("drawcontours", src);
-    waitKey(0);
+    while (true)
+    {
+      capture.read(frame);
+      cv::resize(frame,frame,cv::Size(640,400));
+      if (!frame.empty())
+      {
+        tmp = frame.clone();
+        cv::undistort(frame, tmp, camera_matrix, distortion_coefficients);
+        detect_and_show(tmp);
+      }
+      int c = cv::waitKey(10);
+      if ((char)c == 'q')
+        break;
+    }
   }
-  waitKey(0);
   return 0;
 }
